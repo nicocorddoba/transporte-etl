f2b729cf2fad5e7f6c86c5d0251688ffdecd4378
Nicolás Córdoba <58666053+nicocorddoba@users.noreply.github.com>
2025-04-10 12:31:18 -0300

    Update load.py

--- tasks/load.py
+++ tasks/load.py
@@ -5,5 +5,5 @@ import sqlite3
 
 @task
 def cargar_en_json(lista_datos: list[dict], path: str = "./raw"):
-    json.dumps(lista_datos, indent=4, sort_keys=True, default=str, open(f"{path}/data.json", "w")))
+    json.dumps(lista_datos, indent=4, sort_keys=True, default=str, open(f"{path}/data.json", "w"))
     return "archivo guardado"

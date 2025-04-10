92f9ebc7200008989d3ed566361128076019bc93
Nicolás Córdoba <58666053+nicocorddoba@users.noreply.github.com>
2025-04-10 12:52:17 -0300

    Update load.py

--- tasks/load.py
+++ tasks/load.py
@@ -6,7 +6,7 @@ import datetime
 
 @task
 def cargar_en_json(lista_datos: list[dict], path: str = "./raw"):
-    now_arg = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    now_arg = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     with open(f'{path}/{now_arg}.json', "w", encoding="utf-8") as f:
         json.dump(lista_datos, f, indent=4, default=str)
     return "archivo guardado"

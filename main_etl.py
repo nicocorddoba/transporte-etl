e7afc29ad541df7cf33e2374d13f192255f99fd7
Nicolás Córdoba <58666053+nicocorddoba@users.noreply.github.com>
2025-04-10 13:05:50 -0300

    Update main_etl.py

--- main_etl.py
+++ main_etl.py
@@ -5,12 +5,12 @@ from tasks.transform import transformar_datos
 from tasks.load import cargar_en_json
 
 @flow
-def flujo_transporte(url: str):
+def flujo_transporte(url: str, path: str):
     logger = get_run_logger()
     logger.info("🌱 Iniciando el flujo ETL de posiciones...")
     raw = extraer_datos(url)
     logger.info("Extracted data", raw)
     limpio = transformar_datos(raw)
     logger.info("data cleaned")
-    cej = cargar_en_json(limpio)
+    cej = cargar_en_json(limpio, path)
     logger.info(cej)

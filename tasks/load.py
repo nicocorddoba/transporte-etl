c5785306739e6318a50e5e3a809c5ac6c348d00b
Nicolas Cordoba <nico343@outlook.es>
2025-04-11 16:49:44 -0300

    load modified

--- tasks/load.py
+++ tasks/load.py
@@ -1,9 +1,12 @@
 from prefect import task
 import json
 import sqlite3
-
+from datetime import datetime
+import os
 
 @task
 def cargar_en_json(lista_datos: list[dict], path: str = "./raw"):
-    json.dump(lista_datos, open(path, "w"), indent=4)
+    filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".json"
+    with open(f'{path}\\{filename}', "w", encoding="utf-8") as f:
+        json.dump(lista_datos, f, indent=4, default=str)
     return "archivo guardado"

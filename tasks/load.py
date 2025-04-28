a1ed0367c8dd24f9c5603bcfae96b862fcbacd25
Nicolas Cordoba <nico343@outlook.es>
2025-04-28 19:43:24 -0300

    changed bucket name in load file

--- tasks/load.py
+++ tasks/load.py
@@ -13,21 +13,20 @@ def cargar(path: str = "./raw"):
     file_list = [i.name for i in check]
     if len(file_list) == 0:
         return "No hay archivos para cargar"
-
     stored_data = []
+
     for i in file_list:
         file_path = f'{path}\\{i}'
         data = json.load(open(file_path, 'r'))
         stored_data.append(data)
-        
 
     try: 
         json.dump(stored_data, open(f'{path}\\{today}.json', 'w'), indent=4, default=str)
-        s3 = S3Bucket.load("transporte")
+        s3 = S3Bucket.load("s3block")
         s3.upload_from_path(f'{path}/{today}.json', f'./raw/{today}.json')
     except Exception as e:
         print("Error al guardar el archvio", e)
-        
+
     for i in file_list:
         file_path = f'{path}\\{i}'
         try:
